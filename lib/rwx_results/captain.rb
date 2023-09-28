230974e92ffa0d6c13295eb020942a8815587821
Ryan Schlesinger <ryan@ryanschlesinger.com>
2023-09-28 08:49:28 -0300

    Test debug output

--- lib/rwx_results/captain.rb
+++ lib/rwx_results/captain.rb
@@ -7,17 +7,8 @@ module RwxResults
     required :logger
 
     def call
-      # run_context = RunContext.from_env
-      # logger.debug run_context.to_h
-
-      context.logger.notice(
-        properties: {
-          file: "Dockerfile",
-          line: 70,
-          end_line: 76
-        },
-        message: "That's right! No non-root users allowed!"
-      )
+      run_context = RunContext.from_env
+      logger.debug run_context.to_h
 
       # Fetch captain metadata
       # Generate markdown
